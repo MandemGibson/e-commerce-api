17ecb386c52a0177dbbd0b7d44729e57648f0267
MandemGibson <themaingib@gmail.com>
2024-12-15 23:19:50 +0000

    login and signup handler

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
-import { login } from "../services/auth.service";
+import { login, signUp } from "../services/auth.service";
 import bcrypt from "bcrypt";
+import { hashPassword, validatePassword } from "../utils/password";
 
 export const loginHandler = async (
   req: Request,
@@ -18,7 +19,7 @@ export const loginHandler = async (
     const user = await login(email);
     if (!user) return res.status(401).json({ message: "Invalid email" });
 
-    const isPasswordCorrect = bcrypt.compare(password, user.password);
+    const isPasswordCorrect = await validatePassword(password, user.password);
     if (!isPasswordCorrect)
       return res.status(401).json({ message: "Invalid password" });
 
@@ -35,9 +36,15 @@ export const signUpHandler = async (
 ): Promise<any> => {
   try {
     const { email, password } = req.body;
+    if (!email || !password)
+      return res
+        .status(400)
+        .json({ message: "Email and password are required" });
 
-    //const salt
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await hashPassword(password);
+    const user = await signUp({ email, password: hashedPassword as string });
+
+    res.status(201).json({ message: "Sign up successful", data: user });
   } catch (error: any) {
     return res.status(500).json({ message: error.message });
   }
