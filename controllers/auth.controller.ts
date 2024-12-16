5a69ea448820ac85e35a0b9c08afa4c769307470
MandemGibson <themaingib@gmail.com>
2024-12-16 10:11:22 +0000

    get me and logout handler

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import { login, signUp } from "../services/auth.service";
 import { hashPassword, validatePassword } from "../utils/password";
 import { getUserByEmail } from "../services/user.service";
+import { generateToken } from "../utils/token";
 
 export const loginHandler = async (
   req: Request,
@@ -22,6 +23,8 @@ export const loginHandler = async (
     const isPasswordCorrect = await validatePassword(password, user.password);
     if (!isPasswordCorrect)
       return res.status(401).json({ message: "Invalid password" });
+
+    await generateToken(user.id, res, false);
 
     res.status(200).json({ message: "Login successful", data: user });
   } catch (error: any) {
@@ -49,6 +52,35 @@ export const signUpHandler = async (
     const user = await signUp({ email, password: hashedPassword as string });
 
     res.status(201).json({ message: "Sign up successful", data: user });
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const getMeHandler = async (
+  req: Request,
+  res: Response,
+  _next: NextFunction
+): Promise<any> => {
+  try {
+    const user = req.user;
+    if (!user)
+      return res
+        .status(404)
+        .json({ message: "User not found. Login to continue" });
+    res.status(200).json(user);
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const logoutHandler = async (
+  req: Request,
+  res: Response
+): Promise<any> => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logout successful" });
   } catch (error: any) {
     return res.status(500).json({ message: error.message });
   }
