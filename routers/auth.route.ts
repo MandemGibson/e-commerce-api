206e5d69ce9a925ab822aa7ed0b4d651dd394e31
MandemGibson <themaingib@gmail.com>
2024-12-20 00:28:29 +0000

    update: forgot password endpoint

--- routers/auth.route.ts
+++ routers/auth.route.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import {
+  forgotPasswordHandler,
   getMeHandler,
   loginHandler,
   logoutHandler,
+  resetPasswordHandler,
   signUpHandler,
+  verifyOtpHandler,
 } from "../controllers/auth.controller";
 import { signUpLimiter } from "../middlewares/limiter";
 import { requireUser } from "../middlewares/protectedRoute";
@@ -14,3 +17,6 @@ authRouter.post("/login", loginHandler);
 authRouter.post("/signup", signUpLimiter, signUpHandler);
 authRouter.get("/me", requireUser, getMeHandler);
 authRouter.post("/logout", logoutHandler);
+authRouter.post("/request-otp", forgotPasswordHandler);
+authRouter.post("/verify-otp", verifyOtpHandler);
+authRouter.post("/reset-password", resetPasswordHandler);
