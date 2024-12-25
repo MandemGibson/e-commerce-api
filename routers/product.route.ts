58c4f0e7a72a83033a816940637edb0e4cead8f9
MandemGibson <themaingib@gmail.com>
2024-12-25 21:14:30 +0000

    some product endpoints accessible by admin only

--- routers/product.route.ts
+++ routers/product.route.ts
@@ -7,14 +7,15 @@ import {
   updateProductHandler,
 } from "../controllers/product.controller";
 import { requireUser } from "../middlewares/protectedRoute";
+import { isAdmin } from "../middlewares/role";
 export const productRouter = Router();
 
 productRouter
   .route("/")
   .get(getAllProductsHandler)
-  .post(requireUser, addProductHandler);
+  .post(requireUser, isAdmin, addProductHandler);
 productRouter
   .route("/:productId")
   .get(getProductByIdHandler)
-  .put(requireUser, updateProductHandler)
-  .delete(requireUser, deleteProductHandler);
+  .put(requireUser, isAdmin, updateProductHandler)
+  .delete(requireUser, isAdmin, deleteProductHandler);
