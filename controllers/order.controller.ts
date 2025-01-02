79be5a68d63f930311ac45da0cd26bf62027b3cf
MandemGibson <themaingib@gmail.com>
2025-01-02 21:22:32 +0000

    order controller: get order by id handler

--- controllers/order.controller.ts
+++ controllers/order.controller.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from "express";
-import { getOrdersByUserId, placeOrder } from "../services/order.service";
+import {
+  getOrderById,
+  getOrdersByUserId,
+  placeOrder,
+} from "../services/order.service";
 
 export const placeOrderHandler = async (req: Request, res: Response) => {
   try {
@@ -26,6 +30,21 @@ export const getOrdersByUserIdHandler = async (req: Request, res: Response) => {
       return res.status(400).json({ message: "Failed to get orders" });
 
     return res.status(200).json(orders);
+  } catch (error: any) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+export const getOrderByIdHandler = async (req: Request, res: Response) => {
+  try {
+    const user = req.user;
+    if (!user) return res.status(401).json({ message: "Unauthorized" });
+
+    const { orderId } = req.params;
+    if (!orderId)
+      return res.status(400).json({ message: "Order ID is required" });
+
+    const order = await getOrderById(orderId, user.id as string);
   } catch (error: any) {
     return res.status(500).json({ message: error.message });
   }
