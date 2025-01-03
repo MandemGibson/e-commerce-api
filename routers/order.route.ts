2ab9432d5f1013f42c63d90a66652967fdc0db03
MandemGibson <themaingib@gmail.com>
2025-01-03 20:08:15 +0000

    get order by user id endpoint

--- routers/order.route.ts
+++ routers/order.route.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
-import { placeOrderHandler } from "../controllers/order.controller";
+import {
+  getOrdersByUserIdHandler,
+  placeOrderHandler,
+} from "../controllers/order.controller";
 import { requireUser } from "../middlewares/protectedRoute";
 
 export const orderRouter = Router();
 
 orderRouter.post("/place", requireUser, placeOrderHandler);
+orderRouter.get("/", requireUser, getOrdersByUserIdHandler);
