9a51430248ca0c815360bf8dd698dcb84aed3910
MandemGibson <themaingib@gmail.com>
2025-01-03 20:14:50 +0000

    add update order status endpoint with admin role requirement

--- routers/order.route.ts
+++ routers/order.route.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import {
-    cancelOrderHandler,
+  cancelOrderHandler,
   getOrderByIdHandler,
   getOrdersByUserIdHandler,
   placeOrderHandler,
+  updateOrderStatusHandler,
 } from "../controllers/order.controller";
 import { requireUser } from "../middlewares/protectedRoute";
+import { isAdmin } from "../middlewares/role";
 
 export const orderRouter = Router();
 
@@ -13,3 +15,4 @@ orderRouter.post("/place", requireUser, placeOrderHandler);
 orderRouter.get("/", requireUser, getOrdersByUserIdHandler);
 orderRouter.get("/:orderId", requireUser, getOrderByIdHandler);
 orderRouter.patch("/:orderId/cancel", requireUser, cancelOrderHandler);
+orderRouter.post("/:orderId", requireUser, isAdmin, updateOrderStatusHandler);
